8c4fdc73c436690d0df44bd5e2b72c094b9bf30a
Yujian <yujian_fu@outlook.com>
2020-11-22 09:11:03 +0800

    add files

--- MNIST.py
+++ MNIST.py
@@ -27,7 +27,7 @@ def run_test(parameters, current_time, folder_name):
 def test_conf(base_para, choice_para, name):
     base_para.update(choice_para)
     current_time = datetime.datetime.now().strftime('%b.%d_%H.%M.%S')
-    run_test(base_para, current_time, "cifar_" + name)
+    run_test(base_para, current_time, "mnist_" + name)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='mode')
@@ -39,52 +39,52 @@ if __name__ == "__main__":
 
     if mode == "cen" or mode == "all":
         check_correctness = True
-        logger.info("Testing Centralized Single-Shot on Cifar")
+        logger.info("Testing Centralized Single-Shot on Mnist")
         test_conf(base.parameter_base, cen_sing.parameters, "cen_sing")
 
-        logger.info("Testing Centralized Multi-Shot on Cifar")
+        logger.info("Testing Centralized Multi-Shot on Mnist")
         test_conf(base.parameter_base, cen_multi.parameters, "cen_multi")
 
         
     if mode == "dis" or mode == "all":
         check_correctness = True
-        logger.info("Testing Distributed Single-Shot on Cifar")
+        logger.info("Testing Distributed Single-Shot on Mnist")
         test_conf(base.parameter_base, dis_sing.parameters, "dis_sing")
 
-        logger.info("Testing Distributed Multi-Shot on Cifar")
+        logger.info("Testing Distributed Multi-Shot on Mnist")
         test_conf(base.parameter_base, dis_multi.parameters, "dis_multi")
 
 
     if mode == "diff" or mode == "all":
         check_correctness = True
-        logger.info("Testing Diff-Privacy Single-Shot on Cifar")
+        logger.info("Testing Diff-Privacy Single-Shot on Mnist")
         test_conf(base.parameter_base, diff_privacy_multi_001.parameters, "diff-001")
 
-        logger.info("Testing Distributed Multi-Shot on Cifar")
+        logger.info("Testing Distributed Multi-Shot on Mnist")
         test_conf(base.parameter_base, diff_privacy_multi_002.parameters, "diff-002")
     
     if mode == "double_pix" or mode == "all":
         check_correctness = True
-        logger.info("Testing Double Pixel Single-Shot on Cifar")
+        logger.info("Testing Double Pixel Single-Shot on Mnist")
         test_conf(base.parameter_base, double_pixel_sing.parameters, "double_pix_sing")
 
-        logger.info("Testing Double Pixel Multi-Shot on Cifar")
+        logger.info("Testing Double Pixel Multi-Shot on Mnist")
         test_conf(base.parameter_base, double_pixel_multi.parameters, "double_pix_multi")
     
     if mode == "defense" or mode == "all":
         check_correctness = True
-        logger.info("Testing GeoMedian Single-Shot on Cifar")
+        logger.info("Testing GeoMedian Single-Shot on Mnist")
         test_conf(base.parameter_base, geomedian.parameters, "geomedian")
 
-        logger.info("Testing Double Pixel Multi-Shot on Cifar")
+        logger.info("Testing Double Pixel Multi-Shot on Mnist")
         test_conf(base.parameter_base, foolsgold.parameters, "foolsgold")
 
     if mode == "half_attack" or mode == "all":
         check_correctness = True
-        logger.info("Testing Half Attack Single-Shot on Cifar")
+        logger.info("Testing Half Attack Single-Shot on Mnist")
         test_conf(base.parameter_base, half_attack_sing.parameters, "half_attack_sing")
 
-        logger.info("Testing Half Attack Multi-Shot on Cifar")
+        logger.info("Testing Half Attack Multi-Shot on Mnist")
         test_conf(base.parameter_base, half_attack_multi.parameters, "half_attack_multi")
 
     if not check_correctness:
