22499e77882a95344ddc85757f79240050bc02df
Yujian <yujian_fu@outlook.com>
2020-11-23 00:12:28 +0800

    add files

--- MNIST.py
+++ MNIST.py
@@ -86,11 +86,12 @@ if __name__ == "__main__":
     
     if mode == "defense" or mode == "all":
         check_correctness = True
+        logger.info("Testing Double Pixel Multi-Shot on Mnist")
+        test_conf(base.parameter_base, foolsgold.parameters, "foolsgold")
+
         logger.info("Testing GeoMedian Single-Shot on Mnist")
         test_conf(base.parameter_base, geomedian.parameters, "geomedian")
 
-        logger.info("Testing Double Pixel Multi-Shot on Mnist")
-        test_conf(base.parameter_base, foolsgold.parameters, "foolsgold")
 
     if mode == "half_attack" or mode == "all":
         check_correctness = True
